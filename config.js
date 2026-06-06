14318221a526c469ac5825f2300ceb4061b71536
telitom702-cmd <telitom702@gmail.com>
2026-06-06 23:32:10 +0600

    Update MongoDB URI in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: "mongodb+srv://<username>:<password>@cluster0.mongodb.net/telitask?retryWrites=true&w=majority", // আপনার MongoDB URI দিন
+    MONGO_URI: "mongodb+srv://mongodbpy_db_user:pPgtRKyHsm8GvJF2@cluster0.u2ft5ps.mongodb.net/?appName=Cluster0", // আপনার MongoDB URI দিন
     ADMIN_TELEGRAM_ID: "123456789", // <--- আপনার টেলিগ্রাম নাম্বারিক আইডি এখানে দিন
     PORT: 5000
 };
