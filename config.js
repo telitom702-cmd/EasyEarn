e982f5480d81e42f8ac89af11e66d05f8a3b8fdb
telitom702-cmd <telitom702@gmail.com>
2026-06-06 22:33:09 +0600

    Add configuration for MongoDB and Telegram ID

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-
+module.exports = {
+    MONGO_URI: "mongodb+srv://<username>:<password>@cluster0.mongodb.net/telitask?retryWrites=true&w=majority", // আপনার MongoDB URI দিন
+    ADMIN_TELEGRAM_ID: "123456789", // <--- আপনার টেলিগ্রাম নাম্বারিক আইডি এখানে দিন
+    PORT: 5000
+};
