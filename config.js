fd58beb370ee42c737b49022e7d8720bf0dcd3bd
telitom702-cmd <telitom702@gmail.com>
2026-06-07 21:27:29 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
-d
+module.exports = {
+    // ডাটাবেস নাম (telitask) যোগ করে দেওয়া হয়েছে
+    MONGO_URI: "mongodb+srv://mongodbpy_db_user:pPgtRKyH***@cluster0.u2ft5ps.mongodb.net/?appName=Cluster0", 
+    
+    // ⚠️ এখানে আপনার টেলিগ্রাম নাম্বারিক আইডি দিতে হবে (যেমন: 123456789)
+    ADMIN_TELEGRAM_ID: "8248792819", 
+    
+    PORT: 5000
+};
